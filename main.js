eb70dcbf4ce9cb28ac0f51c2bcbaef5352e17524
Emil Jansson <ejansson.swe@gmail.com>
2021-01-05 14:31:27 +0100

    ✨💡 Add new menu, and some comments

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { BrowserWindow, app, ipcMain, Notification } = require('electron');
+const { BrowserWindow, app, Menu, ipcMain, Notification } = require('electron');
 const path = require('path');
-const url = require('url');
+const isDev = !app.isPackaged;
 
 let mainWindow;
 
@@ -12,18 +12,72 @@ function createBrowserWindow() {
       nodeIntegration: false,
       worldSafeExecuteJavaScript: true,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+    },
   });
 
   mainWindow.loadFile('index.html');
+
+  const mainMenu = Menu.buildFromTemplate(template);
 }
 
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-})
+// reload on save
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+  });
+}
 
-ipcMain.on('notify', (event, {title, body}) => {
+// notifications
+ipcMain.on('notify', (event, { title, body }) => {
   new Notification({ title, body }).show();
 });
+
+// do close application when all windows are closed, except darwin
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// open new window when activated and no windows are present
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createBrowserWindow();
+  }
+});
+
+// create new window when app is readu
 app.whenReady().then(createBrowserWindow);
+
+// default menu
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click: () => {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+// developer menu
+if (isDev) {
+  mainMenuTemplate.push([
+    {
+      label: 'Toggle devtools',
+      accelerator: "CmdOrCtrl+'",
+      click: (item, focusedWindow) => {
+        focusedWindow.toggleDevTools();
+      },
+    },
+    {
+      role: 'reload',
+    },
+  ]);
+}
