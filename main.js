2f5ce17f649d14be6a745a2255fea628b81c16b0
Emil Jansson <ejansson.swe@gmail.com>
2021-01-05 11:02:45 +0100

    ✨ Electron basic configuration

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const { BrowserWindow, app } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createBrowserWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+}
