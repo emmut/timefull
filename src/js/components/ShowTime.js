34bfd66480e1f85f30ffb20c6788d5ddf0f7898d
Emil Jansson <ejansson.swe@gmail.com>
2021-01-06 16:34:46 +0100

    💩 Begin implementing timer functionality

--- src/js/components/ShowTime.js
+++ src/js/components/ShowTime.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
-export default function ShowTime() {
-  return <div>20:10</div>;
+export default function ShowTime({ time }) {
+  // format time
+  function formatTime(number) {
+    return number.toString().padStart(2, '0');
+  }
+  function getMinutes() {
+    return time.minutes;
+  }
+  function getSeconds() {
+    return time.seconds;
+  }
+  if (time.minutes === undefined || time.seconds === undefined) {
+    return null;
+  }
+  return (
+    <span>
+      {time.minutes && getMinutes()} : {time.seconds && getSeconds()}
+    </span>
+  );
 }
--- src/js/routes/Home.js
+++ src/js/routes/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // components
 import ShowIntervals from '../components/ShowIntervals';
@@ -7,15 +7,64 @@ import ShowTime from '../components/ShowTime';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function Home() {
-  const [isStarted, setStartTimer] = useState(false);
+  const mmToMs = (mins) => {
+    return +mins * 60 * 1000;
+  };
+  // TODO: save to localstore
+  const setting = {
+    time: mmToMs(1), // get the time settings
+    interval: mmToMs(5) // get the interval setting
+  };
+
+  // start time
+  const [isStarted, setStart] = useState(false);
+  const [startTime, setStartTime] = useState(0);
+
+  // start the timer
+  useEffect(() => {
+    let id = null;
+    if (isStarted) {
+      setStartTime(setting.time + Date.now());
+      id = setInterval(() => setTimeleft(calculateTimeLeft()), 1000);
+    }
+  }, [isStarted]);
+
+  // calculate time left
+  const calculateTimeLeft = () => {
+    let currentTime = Date.now();
+    const endTime = startTime;
+    const difference = currentTime - endTime;
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        minutes: Math.floor((difference / 60 / 1000) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    }
+    return timeLeft;
+  };
+
+  // initialize time state
+  const [timeLeft, setTimeleft] = useState(calculateTimeLeft());
+
+  // setTimeout
+  // press start
+
+  // loop untill paused
+  //    start timer
+  //    start pause timer
+
   return (
     <div>
       <h1>Im home</h1>
       <ShowIntervals />
-      <button onClick={() => setStartTimer(!isStarted)}>
+      <button onClick={() => setStart(!isStarted)}>
         <FontAwesomeIcon icon={['fas', isStarted ? 'pause' : 'play']} />
       </button>
-      <ShowTime />
+      {/* display clock */}
+      <ShowTime time={timeLeft} />
+      {console.log(timeLeft)}
     </div>
   );
 }
