abfc3e4cf2bf00e8ff33438e92d7f66df4eecb1a
Emil Jansson <ejansson.swe@gmail.com>
2021-01-16 15:46:52 +0100

    💄 Add basic styling for navigation

---
+++ src/js/components/Bars.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export function Bars() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 28 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="2.63708"
+        y1="2.0434"
+        x2="26"
+        y2="2.0434"
+        stroke="#F1F1F1"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <line
+        x1="2.63708"
+        y1="10.9099"
+        x2="26"
+        y2="10.9099"
+        stroke="#F1F1F1"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <line
+        x1="2.63708"
+        y1="19.7763"
+        x2="26"
+        y2="19.7763"
+        stroke="#F1F1F1"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+// Menu hamburger
+import { Bars } from './Bars';
+const Navigation = styled.nav`
+  position: absolute;
+  width: 2.5rem;
+  height: 2.5rem;
+  background: #202030;
+  right: 0;
+  bottom: 0;
+  border-radius: 100vh;
+`;
+
+const NavItems = styled.div`
+  position: relative;
+  top: 0;
+`;
+const Wrap = styled.div`
+  position: absolute;
+  bottom: 100%;
+`;
+const StyledLink = styled(NavLink)`
+  display: inline-block;
+`;
 
 export function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-      <NavLink exact to="/Settings">
-        Settings
-      </NavLink>
-    </nav>
+    <Navigation onClick={() => setIsOpen(!isOpen)}>
+      {isOpen && (
+        <NavItems>
+          <Wrap>
+            <StyledLink exact to="/">
+              Home
+            </StyledLink>
+            <StyledLink exact to="/Settings">
+              Settings
+            </StyledLink>
+          </Wrap>
+        </NavItems>
+      )}
+      <Bars />
+    </Navigation>
   );
 }
