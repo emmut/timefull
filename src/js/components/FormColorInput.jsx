b7dfc6614b584bdfd15fcedfbd584916350e500c
Emil Jansson <ejansson.swe@gmail.com>
2021-07-02 09:57:28 +0200

    🔥 Remove unused state hook

--- src/js/components/FormColorInput.jsx
+++ src/js/components/FormColorInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { shadeColor } from '../lib/helpers';
 import { ColorBox } from './ColorBox';
